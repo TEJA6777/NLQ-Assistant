4307577b90105f86eb41eb46a8f339d820ddf073
sai teja <saitejakodati77@gmail.com>
2025-11-18 08:16:04 +0530

    Fix build script order - migrate before collectstatic

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# build.sh - Render deployment build script
-
 set -e
 
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
-
 echo "Running migrations..."
 python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
 
 echo "Build complete!"
